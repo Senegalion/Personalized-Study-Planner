5bddc0d7cdce20656cd07fb7177011a5689a6467
Lucas <lupelikan@int.pl>
2025-03-05 00:08:02 +0200

    corrected version of .fxml files to work without errors with JDK lower than 23 (Java 17)

--- src/main/java/org/example/personalizedstudyplanner/database/DatabaseUtil.java
+++ src/main/java/org/example/personalizedstudyplanner/database/DatabaseUtil.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 public class DatabaseUtil {
     private static String URL = "jdbc:postgresql://localhost:5432/personalized_study_planner";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "dmbonloz123";
+    private static final String PASSWORD = "postgres";
 
     public static Connection connect() {
         try {
