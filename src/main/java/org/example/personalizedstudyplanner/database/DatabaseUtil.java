2c9800f355a3e45a196186e24aa236c1bf0800dd
Loc Dang <loc.dang@metropolia.fi>
2025-03-04 22:42:32 +0200

    update temp pass to test for jenkins

--- src/main/java/org/example/personalizedstudyplanner/database/DatabaseUtil.java
+++ src/main/java/org/example/personalizedstudyplanner/database/DatabaseUtil.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 public class DatabaseUtil {
     private static String URL = "jdbc:postgresql://localhost:5432/personalized_study_planner";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "postgres";
+    private static final String PASSWORD = "dmbonloz123";
 
     public static Connection connect() {
         try {
